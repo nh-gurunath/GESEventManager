3a0d61d86b34d922eb0c207662d706d24cc8ecbf
nh-gurunath <guru.sx7@gmail.com>
2016-12-17 12:40:24 +0530

    Splash and Login Screen is added

--- app/src/main/java/com/globaledgesoft/eventmanagerges/MainActivity.java
+++ app/src/main/java/com/globaledgesoft/eventmanagerges/MainActivity.java
@@ -1,13 +1,33 @@
 package com.globaledgesoft.eventmanagerges;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent LoginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(LoginIntent);
+                overridePendingTransition(R.animator.fade_in, R.animator.fade_out);
+                finish();
+
+            }
+
+            }, SPLASH_DISPLAY_LENGTH);
     }
 }
