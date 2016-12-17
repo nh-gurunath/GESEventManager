100fee79d3e27cf23cef12e867a6d76d32f2d0dc
Lenovo <sudhigh@gmail.com>
2016-12-17 13:39:27 +0530

    Adding base

--- app/src/main/java/com/globaledgesoft/eventmanagerges/BaseActivity.java
+++ app/src/main/java/com/globaledgesoft/eventmanagerges/BaseActivity.java
@@ -9,5 +9,7 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+
     }
 }
